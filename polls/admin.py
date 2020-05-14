b7e5c75ac339741cf8553638ed44fbc479d5c052
Anders Heilemann <a.heilemann@gmail.com>
2020-05-14 22:21:15 +0200

    Added very basic views, urlconf and admin to the Polls app

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        ChoiceInLine,
+    ]
+
+admin.site.register(Question,QuestionAdmin)
