2258ee7c9e55dc0cf02f8dce8c0338065ef4a3b4
Anders Heilemann <a.heilemann@gmail.com>
2020-05-27 15:33:06 +0200

    Added more info, filters and search in admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-class ChoiceInLine(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
+    extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [
-        ChoiceInLine,
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question,QuestionAdmin)
